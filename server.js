51d54d7f727edd661999b4a645f0ec5ee4d94ba5
christinayan <christina.yan@queensu.ca>
2017-09-23 15:03:21 -0400

    fixed typos

--- server.js
+++ server.js
@@ -1,55 +1,49 @@
+require('dotenv').config()
+
 const http = require('http');
 const express = require('express');
 const twilio = require('twilio');
-
-//const router = require('./router');
+const bodyParser = require('body-parser');
 
 const MessagingResponse = twilio.twiml.MessagingResponse;
-//const AccessToken = twilio.AccessToken;
 
 const app = express();
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.use(router);
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/notifyUser', (req, res, next) => {
+const isAuthorized = true;
+
+app.post('/notifyUser', (req, res) => {
   const twiml = new MessagingResponse();
-  //console.log('request body', req);
-  //console.log('req.body.Body', req.body.Body);
-  console.dir(req.body, { depth: 1 });
-  twiml.message('Unlocked door for registered user');
+  const locked = req.body.Body;
+  if (locked.toLowerCase() === 'yes') {
+	twiml.message('Unlocked door for registered user');
+  } else {
+	  twiml.message('Door remains locked for unauthorized user');
+  }
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
 
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
-
-	const accountSid = 'AC42661db5227293326b8fec88d2a5f9f4'; // Your Account SID from www.twilio.com/console
-	const authToken = '0036159ac127fe864399985d08dfc0f6';   // Your Auth Token from www.twilio.com/console
-	const client = new twilio(accountSid, authToken);
-
-	client.messages.create({
-		to: '+14165797485',  // Text this number
-		from: '+12264551302',
-		body:'Unregistered face detected, unlock door for unknown user?',
-		mediaUrl: 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
-	}, function(error, message) {
-		// The HTTP request to Twilio will run asynchronously. This callback
-		// function will be called when a response is received from Twilio
-		// The "error" constiable will contain error information, if any.
-		// If the request was successful, this value will be "falsy"
-		if (!error) {
-			// The second argument to the callback will contain the information
-			// sent back by Twilio for the request. In this case, it is the
-			// information about the text messsage you just sent:
-			console.log('Success! The SID for this SMS message is:');
-			console.log(message.sid);
-	 
-			console.log('Message sent on:');
-			console.log(message.dateCreated);
-		} else {
-			console.log('Oops! There was an error.');
-		}
+  const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+  let bodyText;
+  if (isAuthorized) {
+	bodyText = 'Registered face detected, unlock door for authorized user?';
+  } else {
+	bodyText = 'Unregistered face detected, unlock door for unauthorized user?';
+  }
+  client.messages.create({
+	to: '+14165797485',  // Text this number
+	from: '+12264551302',
+	body: bodyText,
+	mediaUrl: 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg', //change this to public image url
+  }, function(error, message) {
+	  if (!error) {
+		console.log('Alert sent successfully!');
+	  } else {
+		console.log('Error sending alert!');
+	  }
 	});
 });
 
